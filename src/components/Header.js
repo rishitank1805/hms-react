5f4492ddd0f98b159db56e24d20e0f273b50c6af
Rishi <chiragt1805@gmail.com>
2021-10-24 21:00:01 +0530

    converted to react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 function Header() {
-    return (
-        <div>
-            <header>
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <a href="#" className="logo">
+            <span>H</span>ealth<span>C</span>are.
+          </a>
 
-<div className="container">
+          <nav className="nav">
+            <ul>
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="about">about</Link>
+              </li>
+              <li>
+                <Link to="contact">contact</Link>
+              </li>
+              <li>
+                <Link to="#post">Admin Login</Link>
+              </li>
+            </ul>
+          </nav>
 
-    <a href="#" className="logo"><span>H</span>ealth<span>C</span>are.</a>
-
-    <nav className="nav">
-        <ul>
-            <li><Link to="/">home</Link></li>
-            <li><Link to="about">about</Link></li>
-            <li><Link to="contact.html">contact</Link></li>
-            <li><Link to="#post">Admin Login</Link></li>
-        </ul>
-    </nav>
-
-    <div className="fas fa-bars"></div>
-
-</div>
-
-</header>
-
+          <div className="fas fa-bars"></div>
         </div>
-    )
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
